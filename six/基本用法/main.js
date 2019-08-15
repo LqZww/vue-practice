bf71259ec488f008b2f5db4599a78a96cdae6c43
ZWW <951598077@qq.com>
2019-08-15 15:17:16 +0800

    表单和v-model的用法

---
+++ six/基本用法/main.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        value: '',
+        msg: '',
+        oneradio: false,
+        checkname: '1',
+        checks: [],
+        selected: '',
+        selectedmul: [],
+        picked: true,
+        value: '111',
+        toggle: true,
+        value1: '被选中了',
+        value2: '没有被选中',
+        valueselect: '',
+        inputStr1: '',
+        inputStr2: '',
+        isNum: '',
+        trimStr: ''
+    }
+})
