3fd7b01b4f8c85a1f529737171beb1d6e79c8ba7
ZWW <951598077@qq.com>
2019-12-04 22:46:32 +0800

    添加购物车demo1

---
+++ demo1-购物车/main.js
@@ -0,0 +1,60 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    books: [
+      {
+        id: 1,
+        name: '《算法导论》',
+        date: '2006-9',
+        price: 100.00,
+        count: 1
+      },
+      {
+        id: 2,
+        name: '《JavaScript编程》',
+        date: '2002-2',
+        price: 50.00,
+        count: 1
+      },
+      {
+        id: 3,
+        name: '《Linux系统》',
+        date: '2106-10',
+        price: 70,
+        count: 1
+      },
+      {
+        id: 4,
+        name: '《HTML》',
+        date: '2006-2',
+        price: 120,
+        count: 1
+      }
+    ]
+  },
+  computed: {
+    totalPrices() {
+      let totalPrices = 0
+      for (let i = 0; i < this.books.length; i++) {
+        totalPrices += this.books[i].price * this.books[i].count
+      }
+      return totalPrices
+    }
+  },
+  methods: {
+    reduceCount(index) {
+      this.books[index].count--
+    },
+    addCount(index) {
+      this.books[index].count++
+    },
+    remove(index) {
+      this.books.splice(index, 1)
+    }
+  },
+  filters: {
+    showPrice(price) {
+      return '￥' + price.toFixed(2)
+    }
+  }
+})
